8a0ffc7b58b50f2cedacb0d0feb4cba7256ced69
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2024-08-22 02:15:07 +0300

    finished lsn6

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+import { LoggerService } from '../logger/logger.service';
 import { PostsService } from '../posts/posts.service';
-import { CreateUserDto } from './dto/req/create-user.dto';
-import { UpdateUserDto } from './dto/req/update-user.dto';
+// import { CreateUserDto } from './dto/req/create-user.dto';
+// import { UpdateUserDto } from './dto/req/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly carsService: PostsService) {}
+  constructor(
+    private readonly carsService: PostsService,
+    private readonly logger: LoggerService,
+  ) {}
 
-  public async create(createUserDto: CreateUserDto): Promise<any> {
+  // public async create(createUserDto: CreateUserDto): Promise<any> {
+  public async create(): Promise<any> {
     this.carsService.create({});
+    this.logger.log('This is a test message');
+    throw new Error('This is a test error');
     return 'This action adds a new user';
   }
 
@@ -23,7 +30,7 @@ export class UsersService {
 
   public async updateMe(
     id: number,
-    updateUserDto: UpdateUserDto,
+    // updateUserDto: UpdateUserDto,
   ): Promise<any> {
     return `This action updates a #${id} user`;
   }
