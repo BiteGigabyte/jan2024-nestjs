027eb66e9dd259c61bcf49fdb0f0e3986934f4f1
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2024-08-24 22:37:00 +0300

    finished lsn7-hw

---
+++ src/database/entities/enums/table-name.enum.ts
@@ -0,0 +1,9 @@
+export enum TableNameEnum {
+  LIKES = 'likes',
+  FOLLOWS = 'follows',
+  COMMENTS = 'comments',
+  TAGS = 'tags',
+  ARTICLES = 'articles',
+  REFRESH_TOKENS = 'refresh_tokens',
+  USERS = 'users',
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,12 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 import { LoggerService } from '../logger/logger.service';
 import { PostsService } from '../posts/posts.service';
-import { ArticleRepository } from '../repository/services/article.repository';
 import { UserRepository } from '../repository/services/user.repository';
 import { CreateUserDto } from './dto/req/create-user.dto';
 import { UpdateUserDto } from './dto/req/update-user.dto';
-// import { CreateUserDto } from './dto/req/create-user.dto';
-// import { UpdateUserDto } from './dto/req/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -15,7 +12,6 @@ export class UsersService {
     private readonly carsService: PostsService,
     private readonly logger: LoggerService,
     private readonly userRepository: UserRepository,
-    private readonly articleRepository: ArticleRepository,
   ) {}
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
